81f3408156c210df145768cfe8e47bf4eb8d772f
Pasha0923 <alexsai@i.ua>
2024-07-21 12:56:13 +0300

    fix loader, fix vite.config.js

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,15 +1,15 @@
-import { Circles } from "react-loader-spinner";
+import { ThreeDots } from "react-loader-spinner";
 
 import css from "./Loader.module.css";
 
 const Loader = () => {
   return (
     <div className={css.loader}>
-      <Circles
-        height="100"
-        width="100"
-        color="#e44848"
-        ariaLabel="circles-loading"
+      <ThreeDots
+        height="80"
+        width="80"
+        color="#9be1a0"
+        ariaLabel="hearts-loading"
         wrapperStyle={{}}
         wrapperClass=""
         visible={true}
